748e9d205da5143906a51ae12380591441896af8
deltagphys <cty.jgates@gmail.com>
2019-10-28 10:51:09 -0400

    almost all part 3 passing

--- src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
@@ -8,21 +8,23 @@ public class Cat extends Pet {
      * @param name name of this Cat
      * @param age age of this Cat
      */
-    public Cat(String name, Integer age) {
 
+    public Cat(String name, Integer age) {
+        super(name,age);
     }
 
     /**
      * @param age age of this Cat
      */
     public Cat(Integer age) {
+       super("Cat name", age);
     }
 
     /**
      * @param name name of this Cat
      */
     public Cat(String name) {
-
+        super(name);
     }
 
     /**
@@ -32,12 +34,13 @@ public class Cat extends Pet {
      * age is 0
      */
     public Cat() {
+        super("Cat name");
     }
 
     /**
      * @return meow as a string
      */
     public String speak() {
-        return null;
+        return "Meow";
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/PetOwner.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment1.part3;
 
+import com.zipcodewilmington.assessment1.part1.IntegerArrayUtils;
+import com.zipcodewilmington.assessment1.part2.ArrayUtils;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -8,13 +11,29 @@ public class PetOwner {
      * @param name name of the owner of the Pet
      * @param pets array of Pet object
      */
+    private String name;
+    private Pet[] pets;
+
     public PetOwner(String name, Pet... pets) {
+        this.name = name;
+        this.pets = pets;
     }
 
     /**
      * @param pet pet to be added to the composite collection of Pets
      */
     public void addPet(Pet pet) {
+        // this is gross, but my merge array doesn't work yet
+        if (this.pets != null) {
+            Pet[] petsNew = new Pet[this.pets.length + 1];
+            for (int i = 0; i < this.pets.length; i++) {
+                petsNew[i] = this.pets[i];
+            }
+            petsNew[this.pets.length] = pet;
+        } else {
+            this.pets = new Pet[1];
+            this.pets[0] = pet;
+        }
     }
 
     /**
@@ -29,14 +48,26 @@ public class PetOwner {
      * @return true if I own this pet
      */
     public Boolean isOwnerOf(Pet pet) {
-        return null;
+        if (ArrayUtils.getNumberOfOccurrences(this.pets, pet) > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * @return the age of the Pet object whose age field is the lowest amongst all Pets in this class
      */
     public Integer getYoungetPetAge() {
-        return null;
+        Integer min = Integer.MAX_VALUE;
+        Integer age;
+        for (Pet pet : this.pets) {
+            age = pet.getAge();
+            if (age < min) {
+                min = age;
+            }
+        }
+        return min;
     }
 
 
@@ -46,7 +77,15 @@ public class PetOwner {
      * @return the age of the Pet object whose age field is the highest amongst all Pets in this class
      */
     public Integer getOldestPetAge() {
-        return null;
+        Integer max = 0;
+        Integer age;
+        for (Pet pet : this.pets) {
+            age = pet.getAge();
+            if (age > max) {
+                max = age;
+            }
+        }
+        return max;
     }
 
 
@@ -54,27 +93,32 @@ public class PetOwner {
      * @return the sum of ages of Pet objects stored in this class divided by the number of Pet object
      */
     public Float getAveragePetAge() {
-        return null;
+        Integer[] ages = new Integer[this.pets.length];
+        for (int i = 0; i < this.pets.length; i++) {
+            ages[i] = this.pets[i].getAge();
+        }
+
+        return IntegerArrayUtils.getAverage(ages).floatValue();
     }
 
     /**
      * @return the number of Pet objects stored in this class
      */
     public Integer getNumberOfPets() {
-        return null;
+        return this.pets.length;
     }
 
     /**
      * @return the name property of the Pet
      */
     public String getName() {
-        return null;
+        return this.name;
     }
 
     /**
      * @return array representation of animals owned by this PetOwner
      */
     public Pet[] getPets() {
-        return null;
+        return this.pets;
     }
 }
