4157b9cd8dcf4c0efef09e30ef20641ebee90a98
deltagphys <cty.jgates@gmail.com>
2019-10-28 11:07:49 -0400

    66 of 70

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,28 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
+import java.util.Arrays;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Integer count = 0;
+        int n = input.length();
+        //String[] substrings = new String[n*(n+1)/2];
+        count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i+1; j < input.length()+1; j++) {
+                //substrings[count] =
+                if (isPalindromic(input.substring(i,j))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean isPalindromic(String str) {
+        return str.equals(BasicStringUtils.reverse(str));
     }
 }
